9d7862b3cf03a47861aaddc69683d1582e934ee1
witchhunter <anil.kulkarni@gmail.com>
2018-03-03 13:18:25 +0530

    Updated Reverse String

---
+++ JS/01 Basic Algorithm/01 Reverse A String/reverse.js
@@ -0,0 +1,16 @@
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+reverseString("hello");
+
+/*
+Using the following JS Builtin Objects
+    Global String Object
+
+    String.prototype.split()
+
+    Array.prototype.reverse()
+
+    Array.prototype.join()
+ */
